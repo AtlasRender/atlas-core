779099bb1df4f11d58ca458177c5703724b8cfba
JeyM1 <denafen2000@gmail.com>
2020-10-17 19:14:41 +0300

    [OrganizationsController] Added GET /organizations/:org_id/roles/:role_id - get role data. [OrganizationsController] Added POST /organizations/:org_id/roles/:role_id - edit role data.

--- src/validators/OrganizationRequestValidators.ts
+++ src/validators/OrganizationRequestValidators.ts
@@ -11,7 +11,10 @@
 import {bodyValidator} from "../utils/ajv-middleware/ajv-validation-middleware";
 import {ajvInstance} from "../globals";
 
-
+/**
+ * OrganizationRegisterValidator - validator for organization creating request.
+ * @author Denis Afendikov
+ */
 export const OrganizationRegisterValidator = bodyValidator({
         $id: "OrganizationRegisterValidator",
         type: "object",
@@ -31,6 +34,10 @@ export const OrganizationRegisterValidator = bodyValidator({
     },
     ajvInstance);
 
+/**
+ * OrganizationEditValidator - validator for organization edit request.
+ * @author Denis Afendikov
+ */
 export const OrganizationEditValidator = bodyValidator({
         $id: "OrganizationEditValidator",
         type: "object",
@@ -49,6 +56,10 @@ export const OrganizationEditValidator = bodyValidator({
     },
     ajvInstance);
 
+/**
+ * IncludeBodyUserIdValidator - validator for body userId field in request.
+ * @author Denis Afendikov
+ */
 export const IncludeBodyUserIdValidator = bodyValidator({
         $id: "IncludeBodyUserIdValidator",
         type: "object",
@@ -61,6 +72,10 @@ export const IncludeBodyUserIdValidator = bodyValidator({
     },
     ajvInstance);
 
+/**
+ * RoleAddValidator - validator for role creating request.
+ * @author Denis Afendikov
+ */
 export const RoleAddValidator = bodyValidator({
     $id: "RoleAddValidator",
     type: "object",
@@ -72,10 +87,48 @@ export const RoleAddValidator = bodyValidator({
             minLength: 3,
             maxLength: 50
         },
+        description: {
+            type: "string",
+            maxLength: 255
+        },
         permissionLevel: {
             type: "number",
             minimum: 0,
             maximum: 1000
+        },
+        color: {
+            type: "string",
+            maxLength: 255
+        }
+    }
+}, ajvInstance);
+
+/**
+ * RoleEditValidator - validator for role editing request.
+ * @author Denis Afendikov
+ */
+export const RoleEditValidator = bodyValidator({
+    $id: "RoleEditValidator",
+    type: "object",
+    properties: {
+        name: {
+            // TODO: alphanumeric only
+            type: "string",
+            minLength: 3,
+            maxLength: 50
+        },
+        description: {
+            type: "string",
+            maxLength: 255
+        },
+        permissionLevel: {
+            type: "number",
+            minimum: 0,
+            maximum: 1000
+        },
+        color: {
+            type: "string",
+            maxLength: 255
         }
     }
 }, ajvInstance);
