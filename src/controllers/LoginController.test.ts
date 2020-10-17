d7af6efb8b6819ba155a7377acd8a2c69e8ff361
JeyM1 <denafen2000@gmail.com>
2020-10-17 18:43:26 +0300

    [Testing] Edited LoginController. [OrganizationsController] Added POST /organizations/:org_id - edit org data.

--- src/controllers/LoginController.test.ts
+++ src/controllers/LoginController.test.ts
@@ -9,10 +9,24 @@
 
 import Controller from "../core/Controller";
 import LoginController from "./LoginController";
+import * as request from 'supertest'
+import {server} from "../index";
 
 describe("controllers -> LoginController", () => {
     test("Test if controller is instance of Controller", () => {
         const controller: LoginController = new LoginController();
         expect(controller instanceof Controller).toBe(true);
+    });
+
+    test("Test without body", async () => {
+        const res = await request(server.callback()).post("/login");
+        expect(res.status).toBe(400);
+        expect(res.body.success).toBe(false);
+        expect(res.body.response.name).toBe("AJV_INVALID_PAYLOAD");
+
+    });
+
+    afterAll(() => {
+        server.destroy();
     });
 });
