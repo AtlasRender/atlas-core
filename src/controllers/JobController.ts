f8b4fb9f9320883eba409cbbc2ff945a00acd811
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-11-06 02:33:56 +0200

    [JobController] Added new routes for get job, jobs, task and tasks.

--- src/controllers/JobController.ts
+++ src/controllers/JobController.ts
@@ -18,7 +18,7 @@ import Organization from "../entities/Organization";
 import RenderJob from "../entities/RenderJob";
 import getFramesFromRange from "../utils/getFramesFromRange";
 import {JobSubmitValidator} from "../validators/JobRequestValidators";
-import User from "../entities/User";
+import RenderTask from "../entities/RenderTask";
 
 
 /**
@@ -30,6 +30,34 @@ export default class JobController extends Controller {
     constructor() {
         super("/jobs");
         this.post("/", JobSubmitValidator, this.createJobHandler);
+        this.get("/", this.getJobs);
+        this.get("/:jobId", this.getJob);
+        this.get("/:jobId/tasks", this.getTasks);
+        this.get("/:jobId/tasks/:taskId", this.getTask);
+    }
+
+    /**
+     * checkUserHaveAccessToJob - function, designed check is job available to user.
+     * @param userId - User id
+     * @param jobId - Job id
+     * @author Danil Andreev
+     */
+    public static async checkUserHaveAccessToJob(userId: number, jobId: number) {
+        const jobs: any[] = await RenderJob.getRepository().manager.query(`
+            select *
+            from (
+                     select *
+                     from render_job
+                     where "organizationId" in (
+                         select "organizationId"
+                         from user_organizations
+                         where "userId" = $1
+                     )
+                 ) as jobs
+            where id = $2
+            limit 1
+        `, [userId, jobId]);
+        return !!jobs.length;
     }
 
     /**
@@ -85,5 +113,77 @@ export default class JobController extends Controller {
                 throw error;
             throw new RequestError(503, "Internal server error. Please, visit this resource later.");
         }
+    }
+
+    /**
+     * Route __[get]__ ___/jobs___ - returns all jobs available for user.
+     * @throws RequestError
+     * @method
+     * @author Danil Andreev
+     */
+    public async getJobs(ctx: Context) {
+        const user = ctx.state.user;
+        const jobs = await RenderJob.getRepository().manager.query(`
+            select *
+            from render_job
+            where "organizationId" in (
+                select "organizationId"
+                from user_organizations
+                where "userId" = $1
+            )
+        `, [user.id]);
+        ctx.body = jobs;
+    }
+
+    /**
+     * Route __[GET]__ ___/jobs/:id___ - returns job detailed information.
+     * @code 404, 403
+     * @throws RequestError
+     * @method
+     * @author Danil Andreev
+     */
+    public async getJob(ctx: Context) {
+        const user = ctx.state.user;
+        const {jobId} = ctx.params;
+        if (!await JobController.checkUserHaveAccessToJob(user.id, jobId))
+            throw new RequestError(403, "You don't have permissions to this data.");
+
+        const job = await RenderJob.findOne({where: {id: +jobId}});
+        if (!job)
+            throw new RequestError(404, "Render job not found");
+
+        ctx.body = job;
+    }
+
+    public async getTasks(ctx: Context) {
+        const user = ctx.state.user;
+        const {jobId} = ctx.params;
+
+        if (!await JobController.checkUserHaveAccessToJob(user.id, jobId))
+            throw new RequestError(403, "You don't have permissions to this data.");
+
+        const job = await RenderJob.findOne({where: {id: jobId}});
+        if (!job)
+            throw new RequestError(404, "Render job not found");
+
+        const tasks = await RenderTask.find({where: {job}});
+        if (!tasks)
+            throw new RequestError(404, "Render job not found");
+
+        ctx.body = tasks;
+    }
+
+    public async getTask(ctx: Context) {
+        const user = ctx.state.user;
+        const {jobId, taskId} = ctx.params;
+
+        if (!await JobController.checkUserHaveAccessToJob(user.id, jobId))
+            throw new RequestError(403, "You don't have permissions to this data.");
+
+        const task = await RenderTask.findOne({where: {id: +taskId}});
+        if (!task)
+            throw new RequestError(404, "Render task not found");
+
+        ctx.body = task;
     }
 }
