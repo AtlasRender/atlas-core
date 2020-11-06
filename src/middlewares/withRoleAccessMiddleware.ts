d337df0cf753a774d2c0abda66368697aabc1eee
JeyM1 <denafen2000@gmail.com>
2020-11-06 20:44:20 +0200

    [withRoleAccessMiddleware] Changed logic, removed copy-pasting.

--- src/middlewares/withRoleAccessMiddleware.ts
+++ src/middlewares/withRoleAccessMiddleware.ts
@@ -11,6 +11,7 @@ import {Context, Middleware, Next} from "koa";
 import {getRepository} from "typeorm";
 import User from "../entities/User";
 import RequestError from "../errors/RequestError";
+import Role from "../entities/Role";
 
 
 /**
@@ -31,34 +32,70 @@ const findLoggedUser = async (ctx: Context): Promise<User> => {
         throw new RequestError(403, "You are not a member of this organization.");
     }
     return user;
-}
+};
+
+/**
+ * withRoleAccessOrOwner - utility function, used for checking role access.
+ * @param rolePredicate - predicate used in _roles.some()_. Specifies the field on which checking will be based.
+ */
+const withRoleAccessOrOwner = (
+    rolePredicate: (value: Role, index: number, array: Role[]) => unknown
+): Middleware =>
+    async (ctx: Context, next: Next): Promise<void> => {
+        const user = await findLoggedUser(ctx);
+        const hasAccess = user.roles.some(rolePredicate);
+        // check if user has permission for this
+        if (!hasAccess && user.id !== ctx.state.organization.ownerUser.id) {
+            throw new RequestError(403, "Forbidden.");
+        }
+        await next();
+    };
 
 /**
  * canManageRoles - Koa.Middleware for checking if logged user has permission to manage roles.
  * @param ctx - Context
  * @param next - Next
  */
-export const canManageRoles: Middleware = async (ctx: Context, next: Next) => {
-    const user = await findLoggedUser(ctx);
-    const canManageRoles = user.roles.some(role => role.canManageRoles);
-    // check if user has permission to manage roles
-    if (!canManageRoles && user.id !== ctx.state.organization.ownerUser.id) {
-        throw new RequestError(403, "Forbidden.");
-    }
-    await next();
-};
+export const canManageRoles: Middleware = withRoleAccessOrOwner(role => role.canManageRoles);
 
 /**
  * canManageUsers - Koa.Middleware for checking if logged user has permission to manage users.
  * @param ctx - Context
  * @param next - Next
  */
-export const canManageUsers: Middleware = async (ctx: Context, next: Next) => {
-    const user = await findLoggedUser(ctx);
-    const canManageUsers = user.roles.some(role => role.canManageUsers);
-    // check if user has permission to manage users
-    if (!canManageUsers && user.id !== ctx.state.organization.ownerUser.id) {
-        throw new RequestError(403, "Forbidden.");
-    }
-    await next();
-};
+export const canManageUsers: Middleware = withRoleAccessOrOwner(role => role.canManageUsers);
+
+/**
+ * canManageTeams - Koa.Middleware for checking if logged user has permission to manage teams.
+ * @param ctx - Context
+ * @param next - Next
+ */
+export const canManageTeams: Middleware = withRoleAccessOrOwner(role => role.canManageTeams);
+
+/**
+ * canManagePlugins - Koa.Middleware for checking if logged user has permission to manage plugins.
+ * @param ctx - Context
+ * @param next - Next
+ */
+export const canManagePlugins: Middleware = withRoleAccessOrOwner(role => role.canManagePlugins);
+
+/**
+ * canCreateJobs - Koa.Middleware for checking if logged user has permission to create jobs.
+ * @param ctx - Context
+ * @param next - Next
+ */
+export const canCreateJobs: Middleware = withRoleAccessOrOwner(role => role.canCreateJobs);
+
+/**
+ * canEditJobs - Koa.Middleware for checking if logged user has permission to edit jobs.
+ * @param ctx - Context
+ * @param next - Next
+ */
+export const canEditJobs: Middleware = withRoleAccessOrOwner(role => role.canEditJobs);
+
+/**
+ * canDeleteJobs - Koa.Middleware for checking if logged user has permission to delete jobs.
+ * @param ctx - Context
+ * @param next - Next
+ */
+export const canDeleteJobs: Middleware = withRoleAccessOrOwner(role => role.canDeleteJobs);
