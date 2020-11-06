6edc15ab637c78041a0d2a4bd22eb01bb8a5b96d
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-11-06 15:05:45 +0200

    [globals] AMQP queue names turned back to production.

--- src/globals.ts
+++ src/globals.ts
@@ -13,19 +13,19 @@ import exp = require("constants");
 /**
  * AMQP_CONNECTION_QUEUE - queue name for slaves connection reports.
  */
-export const AMQP_CONNECTION_QUEUE = "slave_connection1"
+export const AMQP_CONNECTION_QUEUE = "slave_connection"
 /**
  * AMQP_TASKS_QUEUE - queue name for render tasks.
  */
-export const AMQP_TASKS_QUEUE = "render_tasks1"
+export const AMQP_TASKS_QUEUE = "render_tasks"
 /**
  * AMQP_REPORTS_QUEUE - queue name for slaves runtime reports.
  */
-export const AMQP_TASK_REPORTS_QUEUE = "slave_reports1"
+export const AMQP_TASK_REPORTS_QUEUE = "slave_reports"
 /**
  * AMQP_JOBS_QUEUE - queue name for hobs management.
  */
-export const AMQP_JOBS_QUEUE = "jobs1";
+export const AMQP_JOBS_QUEUE = "jobs";
 
 export const ajvInstance = new Ajv({
     allErrors: true,
