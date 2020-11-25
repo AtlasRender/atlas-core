2dcbf8ffe0cd23ac1e477a7da3ad38ce34528fa4
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-11-25 13:26:53 +0200

    [Entities] Added JSDoc for several entities.

--- src/entities/Organization.ts
+++ src/entities/Organization.ts
@@ -33,48 +33,76 @@ import {Moment} from "moment";
  */
 @Entity()
 export default class Organization extends BaseEntity {
-
     @PrimaryGeneratedColumn()
     id: number;
 
+    /**
+     * name - name - organization name. Must be unique.
+     */
     @Column({unique: true})
     name: string;
 
+    /**
+     * description - organization description.
+     */
     @Column({type: "text", nullable: true})
     description: string;
 
+    /**
+     * ownerUser - the owner of the organization.
+     */
     @ManyToOne(type => User)
     @JoinColumn()
     ownerUser: User;
 
+    /**
+     * defaultRole - default role in the organization. Will be assigned by default for all newcomers.
+     */
     @OneToOne(type => Role, {nullable: true})
     @JoinColumn()
     defaultRole: Role;
 
+    /**
+     * users - organization members.
+     */
     @ManyToMany(type => User, user => user.organizations)
     @JoinTable({
         name: "user_organizations"
     })
     users: User[];
 
+    /**
+     * slaves - slaves, connected to this organization.
+     */
     @ManyToMany(type => Slave, slave => slave.organizations)
     @JoinTable({
         name: "slaves_pool"
     })
     slaves: Slave[];
 
+    /**
+     * jobs - all render jobs, that belongs to this organization.
+     */
     @OneToMany(type => RenderJob, job => job.organization)
     jobs: RenderJob[];
 
+    /**
+     * roles - all roles in organization. Roles can be assigned to members to give rights for doing something.
+     */
     @OneToMany(type => Role, role => role.organization)
     roles: Role[];
 
+    /**
+     * logs - organization log.
+     */
     @OneToMany(type => OrganizationLog, log => log.organization)
     logs: OrganizationLog[];
 
+    /**
+     * plugins - plugins, that the organization contains.
+     */
     @OneToMany(type => Plugin, plugin => plugin.organization)
     plugins: Plugin[];
-
 
     @CreateDateColumn()
     createdAt: Moment;
