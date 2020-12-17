1c4b10a38bc5dd61146df82f59075df63f17a7c7
JeyM1 <denafen2000@gmail.com>
2020-12-17 21:51:43 +0200

    [OrganizationController] Added route for getting user permissions. [Interfaces] Added UserWithPermissions interface.

---
+++ src/interfaces/UserWithPermissions.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020. This code created and belongs to Atlas render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * Project: atlas-core
+ * File last modified: 17.12.2020, 21:43
+ * All rights reserved.
+ */
+
+import User from "../entities/User";
+
+
+export interface UserPermissions {
+    canManageUsers: number,
+    canManageRoles: number,
+    canCreateJobs: number,
+    canDeleteJobs: number,
+    canEditJobs: number,
+    canManagePlugins: number,
+    canManageTeams: number,
+    canEditAudit: number,
+}
+
+export interface UserWithPermissions extends User {
+    permissions?: UserPermissions
+}
