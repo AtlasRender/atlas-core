eb1fd29ac3b94c996e5f8818db86276716502cd1
JeyM1 <denafen2000@gmail.com>
2021-01-07 11:14:26 +0200

    [jest] Moved config to package.json. Added beforeAll tests function.

---
+++ tests/mocks/runtime.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021. This code created and belongs to Atlas render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * Project: atlas-core
+ * File last modified: 07.01.2021, 10:59
+ * All rights reserved.
+ */
+
+export default async function () {
+    console.log("Hello world!");
+}
